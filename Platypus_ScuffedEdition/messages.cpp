6b1e212d5a91a8dd956ac0e3055faf6c8b6392c3
Muhammad Ryan Fikri Fakhrezi <ryanfikri5@gmail.com>
2023-08-04 21:19:28 +0700

    another modification in messages

--- Platypus_ScuffedEdition/messages.cpp
+++ Platypus_ScuffedEdition/messages.cpp
@@ -4,10 +4,10 @@ sf::Font text::font;
 
 sf::Text text::bulletEmpty() {
 	sf::Text text;
-	font.loadFromFile("fonts/SAOUITT-Regular.ttf");
+	font.loadFromFile("fonts/Poppins-SemiBold.ttf");
 	text.setFont(font);
 	text.setString("The bullet is empty\nPress X to reload the bullet");
-	text.setCharacterSize(100);
+	text.setCharacterSize(80);
 	text.setFillColor(sf::Color::Black);
 
 	return text;
@@ -15,10 +15,10 @@ sf::Text text::bulletEmpty() {
 
 sf::Text text::lose() {
 	sf::Text text;
-	font.loadFromFile("fonts/SAOUITT-Regular.ttf");
+	font.loadFromFile("fonts/Poppins-SemiBold.ttf");
 	text.setFont(font);
 	text.setString("YOU LOSE\nBETTER LUCK NEXT TIME\npress 'r' to restart");
-	text.setCharacterSize(100);
+	text.setCharacterSize(80);
 	text.setFillColor(sf::Color::Black);
 
 	return text;
@@ -26,11 +26,11 @@ sf::Text text::lose() {
 
 sf::Text text::startLevel(int level) {
 	sf::Text text;
-	font.loadFromFile("fonts/SAOUITT-Regular.ttf");
+	font.loadFromFile("fonts/Poppins-SemiBold.ttf");
 	text.setFont(font);
 	std::string _string = "LEVEL " + std::to_string(level) + '\n' + "DO YOU READY?\npress 'c' to continue";
 	text.setString(_string);
-	text.setCharacterSize(100);
+	text.setCharacterSize(80);
 	text.setFillColor(sf::Color::Black);
 
 	return text;
