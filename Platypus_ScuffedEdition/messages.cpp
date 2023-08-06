e9dbd6c59447747a48ef8cceb9d37cb0af3a9848
Muhammad Ryan Fikri Fakhrezi <ryanfikri5@gmail.com>
2023-08-06 19:55:03 +0700

    nothing changed, just deleting some unnecessary lines

--- Platypus_ScuffedEdition/messages.cpp
+++ Platypus_ScuffedEdition/messages.cpp
@@ -4,11 +4,10 @@ sf::Font text::font;
 
 sfe::RichText text::bulletEmpty() {
 	font.loadFromFile("fonts/Poppins-SemiBold.ttf");
-
 	sfe::RichText text(font);
 
 	text << sf::Color::Black << "The bullet is empty\n"
-		<< sf::Color::Black << "Press 'X' to reload the bullet";
+		<< sf::Color::Black << "Press 'C' to reload the bullet";
 
 	text.setCharacterSize(50);
 	text.setPosition(30, 560);
@@ -18,7 +17,6 @@ sfe::RichText text::bulletEmpty() {
 
 sfe::RichText text::lose() {
 	font.loadFromFile("fonts/Poppins-SemiBold.ttf");
-
 	sfe::RichText text(font);
 
 	text << sf::Color::Black << "YOU LOSE\nBETTER LUCK NEXT TIME"
@@ -31,11 +29,10 @@ sfe::RichText text::lose() {
 
 sfe::RichText text::startLevel(int level) {
 	font.loadFromFile("fonts/Poppins-SemiBold.ttf");
-
 	sfe::RichText text(font);
 
 	text << sf::Color::Black << "LEVEL " << sf::Color::Blue << std::to_string(level)
-		<< sf::Color::Black << "\nPress 'C' to continue";
+		<< sf::Color::Black << "\nPress ENTER to continue";
 
 	text.setCharacterSize(60);
 	return text;
@@ -43,7 +40,6 @@ sfe::RichText text::startLevel(int level) {
 
 sfe::RichText text::score(float point) {
 	font.loadFromFile("fonts/Poppins-SemiBold.ttf");
-
 	sfe::RichText text(font);
 
 	text << sf::Color::Black << "Score: " << sf::Color::Blue << std::to_string(point);
@@ -54,7 +50,6 @@ sfe::RichText text::score(float point) {
 
 sfe::RichText text::startMenuChoice(int choice) {
 	font.loadFromFile("fonts/Poppins-SemiBold.ttf");
-
 	sfe::RichText text(font);
 	switch (choice) {
 	case 0:
